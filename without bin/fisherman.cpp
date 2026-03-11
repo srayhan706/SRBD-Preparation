e4bea47a798b3601c71afa3e60641df1f914e3ca
srayhan706 <srayhan706@gmail.com>
2026-03-11 22:20:34 +0600

    updated

--- without bin/fisherman.cpp
+++ without bin/fisherman.cpp
@@ -1,52 +1,85 @@
 #include <iostream>
 #include <vector>
-#include <climits>
-#define ll long long
-#define pub push_back
+#include <algorithm>
 using namespace std;
-int n;
-int a[20], b[20];
-void back()
-{
-    int finalAns = INT_MAX;
-    for (int i = 1; i <= n - (b[0] + b[1] + b[2]) + 1; i++)
-    {
-        for (int j = i + b[0]; j <= n - (b[1] + b[2]) + 1; j++)
-        {
-            for (int k = j + b[1]; k <= n - (b[2]) + 1; k++)
-            {
-                int ans = 0;
-                for (int p = i; p < i + b[0]; p++)
-                {
-                    ans += (abs(a[0] - p) + 1);
-                }
-                for (int p = j; p < j + b[1]; p++)
-                {
-                    ans += (abs(a[1] - p) + 1);
-                }
-                for (int p = k; p < k + b[2]; p++)
-                {
-                    ans += (abs(a[2] - p) + 1);
-                }
-                // cout << i << " " << j << " " << k << " " << ans << endl;
-                finalAns = min(ans, finalAns);
-            }
-        }
+
+int N;
+int gate[3];
+int fisher[3];
+int ans=INT_MAX;
+bool used[11];
+
+void dfs(int idx,int order[],int distsum){
+
+    if(idx==3){
+        ans=min(ans,distsum);
+        return;
     }
-    cout << finalAns << endl;
+
+    int g=order[idx];
+    int pos=gate[g];
+    int f=fisher[g];
+
+
+
+    function<void(int,int)> place = [&](int cnt,int sum){
+        if(cnt==f){
+            dfs(idx+1,order,distsum+sum);
+            return;
+        }
+
+        int d=0;
+
+        while(true){
+            bool leftvalid=false,rightvalid=false;
+            int l=pos-d;
+            int r=pos+d;
+
+            if(l>=1 && !used[l]){
+                leftvalid=true;
+            }
+            if(r<=N && !used[r] && d !=0){
+                rightvalid=true;
+            }
+
+            if(leftvalid){
+                used[l]=true;
+                place(cnt+1,sum+d+1);
+                used[l]=false;
+            }
+            if(rightvalid){
+                used[r]=true;
+                place(cnt+1,sum+d+1);
+                used[r]=false;
+            }
+
+            if(leftvalid || rightvalid){
+                break;
+            }
+            d++;
+        }
+    
+    };
+    place(0,0);
+
 }
-void solve()
-{
-    cin >> n;
-    for (int i = 0; i < 3; i++)
-        cin >> a[i];
-    for (int i = 0; i < 3; i++)
-        cin >> b[i];
-    back();
-}
-int main()
-{
-  
-    solve();
-    return 0;
+
+int main(){
+    cin>>N;
+
+    for(int i=0;i<3;i++){
+        cin>>gate[i];
+    }
+
+    for(int i=0;i<3;i++){
+        cin>>fisher[i];
+    }
+
+    int order[3]={0,1,2};
+    do{
+        memset(used,false,sizeof(used));
+        dfs(0,order,0);
+    }while(next_permutation(order,order+3));
+
+    cout<<ans<<endl;
 }
